56d6c6cfe65002e51a911a7d1c12432bf0fa8295
Tom McKenna <tom2mckenna@gmail.com>
2023-10-28 12:57:51 +0100

    revert main branch back to linux version

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # terminal-flappy-bird
-Play a flappy bird game in the terminal.
+Play a flappy bird game in the terminal. THIS PROJECT IS CURRENTLY BROKEN AND IN THE PROCESS OF BEING FIXED.
--- main.c
+++ main.c
@@ -1,16 +1,8 @@
+#include "linuxterminal.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <linux/ioctl.h>
 #include <signal.h>
-#include <termios.h>
 #include <time.h> 
-
-struct winsize terminal_size;
-
-void set_win_size(){
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &terminal_size);
-}
 
 struct game_object {
     float x;
@@ -18,40 +10,21 @@ struct game_object {
     int s;
 };
 
-struct termios linux_get_terminal_settings() {
-    struct termios terminal_settings
-    tcgetattr(STDIN_FILENO, &terminal_settings);
-    return terminal_settings;
-}
-
-void linux_set_terminal_settings(struct termios terminal_settings) {
-    struct termios new;
-	new = terminal_settings;
-	// Disable canonical mode and input echo
-	new.c_lflag &= (~ICANON & ~ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &new);
-}
-
-int check_user_input() {
-    int bytesWaiting;
-    ioctl(STDIN_FILENO, FIONREAD, &bytesWaiting);
-    return bytesWaiting;
-}
+struct terminal terminal;
 
 void draw_current_game_frame(struct game_object bird, struct game_object *pipes, int NUMBER_OF_PIPES, int pipe_opening) {
-    int number_of_rows = terminal_size.ws_row;
-    int number_of_columns = terminal_size.ws_col; 
+    terminal = get_terminal_size();
 
     // Move cursor to top left of screen (so new game frame overwrites current screen)
     printf("\033[H");
 
-    for(int row = 1; row <= number_of_rows; row++){
-        for(int column = 1; column <= number_of_columns; column++) {
+    for(int row = 1; row <= terminal.rows; row++){
+        for(int column = 1; column <= terminal.cols; column++) {
             //Draw border
-            if(row == 1 || row == number_of_rows) {
+            if(row == 1 || row == terminal.rows) {
                 putchar('-');
             }
-            else if(column == 1 || column == number_of_columns) {
+            else if(column == 1 || column == terminal.cols) {
                 putchar('|');
             }
             //Draw bird
@@ -68,7 +41,7 @@ void draw_current_game_frame(struct game_object bird, struct game_object *pipes,
                 int checker = 1;
                 for(int i = 0; i < NUMBER_OF_PIPES; i++){
                     if(pipes[i].x == column) {
-                        if(row <= pipes[i].y*(terminal_size.ws_row-1-pipe_opening) || row >= (pipes[i].y*(terminal_size.ws_row-1-pipe_opening))+pipe_opening){
+                        if(row <= pipes[i].y*(terminal.rows-1-pipe_opening) || row >= (pipes[i].y*(terminal.rows-1-pipe_opening))+pipe_opening){
                             putchar('#');
                             checker = 0;
                             break;
@@ -98,23 +71,21 @@ int main(int argc, char **argv){
 
     // Set random seed to current time to ensure new pipe y values each execution
     srand(time(NULL));
-
-    signal(SIGWINCH, set_win_size);
-    set_win_size();
+    terminal = get_terminal_size();
     
-    bird.x = terminal_size.ws_col/8;
-    bird.y = (terminal_size.ws_row/3)*2;
+    bird.x = terminal.cols/8;
+    bird.y = (terminal.rows/3)*2;
     bird.s = 0;
     for(int i = 0; i < NUMBER_OF_PIPES; i++){
-        pipes[i].x = terminal_size.ws_col/3 + i*25;
+        pipes[i].x = terminal.cols/3 + i*25;
 
         pipes[i].y = rand()/(float)RAND_MAX;
     }
 
     draw_current_game_frame(bird, pipes, NUMBER_OF_PIPES, PIPE_GAP);
 
-    struct termios old = linux_get_terminal_settings();
-    linux_set_terminal_settings()
+    struct termios old = get_terminal_settings();
+    set_terminal_settings(old);
 
     int birdClock = 0;
     int pipeClock = 0;
@@ -155,8 +126,8 @@ int main(int argc, char **argv){
             // Move bird down according to bird fall speed
             if(birdClock == BIRD_FALL_SPEED){
                 // Stop bird at bottom edge of game window
-                if(bird.y > terminal_size.ws_row-2){
-                    bird.y = terminal_size.ws_row-1;
+                if(bird.y > terminal.rows-2){
+                    bird.y = terminal.rows-1;
                 }
                 else{
                     bird.y++;
@@ -178,7 +149,7 @@ int main(int argc, char **argv){
         struct timespec ts;
         ts.tv_sec = FRAME_DELAY / 1000;
         ts.tv_nsec = (FRAME_DELAY % 1000) * 1000000;
-        // nanosleep(&ts, &ts);
+        nanosleep(&ts, &ts);
     }
 
     // Restore old terminal settings
